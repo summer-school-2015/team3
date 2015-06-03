eea238c0a98508a2bc38990104ac7dccd35e6bb5
Temnova <natasha-98@inbox.ru>
2015-06-03 12:12:22 +0300

    helloex

---
+++ com/temnova/helloex/HelloWorldApp1.java
@@ -0,0 +1,6 @@
+package com.temnova.helloex;
+public class HelloWorldApp1 {
+    public static void main(String[] args) {
+        Printer.printere();
+    }
+}
---
+++ com/temnova/helloex/Printer.java
@@ -0,0 +1,6 @@
+package com.temnova.helloex;
+public class Printer {
+    public static void printere() {
+        System.out.println("Hello World!");
+    }
+}
